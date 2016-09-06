eef0d11268e1c9a5f3f0dad2fd9dbe5557a51adf
Casper <y.mikhaylova@velvetech.ru>
2016-09-06 13:05:40 +0700

    added bootstrap

--- AdminPortal/config/routes.rb
+++ AdminPortal/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
+
   resources :equipment
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
